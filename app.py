d98bea6d967c8c33e4eee512ab4ad5eba6edd335
Jonas Galvez <jonasgalvez@gmail.com>
2017-06-09 06:01:09 -0300

    Load nutrient chart

--- app.py
+++ app.py
@@ -2,12 +2,15 @@
 # coding: utf-8
 
 from flask import Flask, render_template
+from fabfile import nutrients
 
 app = Flask(__name__, template_folder='.')
 
 @app.route('/')
 def home():
-	return render_template('app.html')
+	return render_template('app.html', **{
+		'nutrients': nutrients()['nutrients']
+	})
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=9000, debug=True)
