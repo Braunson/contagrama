c0de39057c82521f0a0808bb6e996b65274d32e2
Jonas Galvez <jonasgalvez@gmail.com>
2018-06-03 17:04:18 -0300

    exportNutrients() -> parseNutrients()

--- src/nutrients.js
+++ src/nutrients.js
@@ -1,5 +1,7 @@
 import nutrients from '~/nutrients.txt'
 
+const NUTRIENT_REGEX = /^(.*?)\s(?:\(((?:k|(?:mc?)|(?:\xb5)?g)|(?:IU)|(?:kcal)|(?:kJ))\))?$/
+
 export const loadNutrients = () => {
   const parsed = []
   let nid = 1
@@ -35,34 +37,32 @@ export const loadNutrients = () => {
   }
 }
 
-export const exportNutrients = (arr) => {
+export const parseNutrients = (arr) => {
   const ndb = []
+  let group
+  let subgroup
+  let ngroup
+  let nutrient
   let nextId = 1
-  for group in db['nutrients']:
-        ngroup = []
-        for subgroup in group:
-            nutrient = re.search(
-                (
-                    '^(.*?)\s*'
-                    '(?:\(('
-                        '(?:k|(?:mc?)|(?:\xb5)?g)|'
-                        '(?:IU)|'
-                        '(?:kcal)|'
-                        '(?:kJ)'
-                    ')\))?$'
-                ),
-                subgroup
-            )
-            if nutrient:
-                nutrient = list(nutrient.groups())
-                if len(nutrient) < 2:
-                    nutrient.append(None)
-                ngroup.append((nextId, nutrient[0], nutrient[1]))
-            else:
-                print('Failed: ', subgroup)
-            nextId += 1
-        if len(ngroup) > 1:
-            ndb.append(ngroup)
-    ndb = {'nutrients': ndb}
-    pprint.pprint(ndb)
-    return ndb
+  for (let i = 0, ilen = arr.length; i < ilen; i++) {
+    group = arr[i]
+    ngroup = []
+    for (let k = 0, klen = group.length; k < klen; k++) {
+      subgroup = group[i][k]
+      nutrient = subgroup.match(NUTRIENT_REGEX)
+      if (nutrient) {
+        nutrient = nutrient.slice(1)
+        if (nutrient.length < 2) {
+          nutrient.push(null)
+          ngroup.push([nextId, nutrient[0], nutrient[1]])
+        } else {
+          console.log('Failed: ', subgroup)
+        }
+        nextId += 1
+        if (ngroup.length > 1) {
+          ndb.push(ngroup)
+        }
+      }
+  }
+  return ndb
+}
