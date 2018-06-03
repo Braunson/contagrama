07b4242fb74eeb37229a77810c9d2f3f719e82e2
Jonas Galvez <jonasgalvez@gmail.com>
2018-06-03 16:07:28 -0300

    Added parseNutrients.js (3)

--- src/parseNutrients.js
+++ src/parseNutrients.js
@@ -10,23 +10,27 @@ export const parseNutrients = () => {
   const lines = nutrients.split(/\n/)
   for (let i = 0, len = lines.length; i < len; i++) {
     line = lines[i]
-            if not re.search('^\s+', line):
+    if (!line.match(/^\s+/)) {
+        line = line.replace('★', '').trim()
+        parsed.push([line])
+        group = parsed[parsed.length - 1]
+    } else if (group !== null) {
+        if not re.search('^\t\t', line) {
+            if '\xe2\x98\x85' in line: {
                 line = line.replace('\xe2\x98\x85', '').strip()
-                db['nutrients'].append([line.decode('utf-8')])
-                group = db['nutrients'][-1]
-            elif group is not None:
-                if not re.search('^\t\t', line):
-                    if '\xe2\x98\x85' in line:
-                        line = line.replace('\xe2\x98\x85', '').strip()
-                        group.append(line.decode('utf-8'))
-                    subgroup_started = False                    
-                else:
-                    if not subgroup_started:
-                        subgroup = group[-1] = [group[-1]]
-                        subgroup_started = True
-                    if '\xe2\x98\x85' in line:
-                        line = line.replace('\xe2\x98\x85', '').strip()
-                        subgroup.append(line.decode('utf-8'))
+                group.append(line.decode('utf-8'))
+            }
+            subgroup_started = False                    
+        } else {
+            if not subgroup_started:
+                subgroup = group[-1] = [group[-1]]
+                subgroup_started = True
+            if '\xe2\x98\x85' in line {
+                line = line.replace('\xe2\x98\x85', '').strip()
+                subgroup.append(line.decode('utf-8'))
+            }
+        }
+    }
     ndb = []
     nextId = 1
     for group in db['nutrients']:
