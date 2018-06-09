6d754557e98a92923ef765f790540fb9fc9409d3
Jonas Galvez <jonasgalvez@gmail.com>
2018-06-09 19:37:40 -0300

    Ongoing work on foods/

---
+++ src/pages/foods.vue
@@ -0,0 +1,12 @@
+<template>
+</template>
+
+<script>
+export default {
+  asyncData ({ app, query }) {
+  	return {
+      foods: await app.$axios.post('/api/food/list', { page: 1 })
+  	}
+  }
+}
+</script>
