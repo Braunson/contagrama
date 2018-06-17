8229b0defb51e75dbb7cc59e35663deb2d53ea49
Jonas Galvez <jonasgalvez@gmail.com>
2018-06-17 13:48:10 -0300

    Added missing termsHash reset (2)

--- src/pages/foods/index.vue
+++ src/pages/foods/index.vue
@@ -156,7 +156,7 @@ export default {
       const foodsResponse = await this.$axios.post('foods/paginate', { filters, page })
       this.foods = foodsResponse.data.rows
       this.termsHashOrig = genTermsHash(this.foods)
-      this.termsHash = genTermsHash(foodsResponse.data.rows)
+      this.termsHash = genTermsHash(this.foods)
       this.totalPages = foodsResponse.data.total_pages
       this.page = page
       this.updatePath()
